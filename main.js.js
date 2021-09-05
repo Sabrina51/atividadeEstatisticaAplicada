8fad28bc79f6796040457e1689cad94e306567f3
sabrina51 <sabrina.silva51@fatec.sp.gov.br>
2021-09-05 12:48:06 -0300

    code actualization

--- main.js.js
+++ main.js.js
@@ -1,45 +1,64 @@
-var conjuntoNumero = [10,9,9,7,5,8,6,9,10,7];
+// 1 verificar a quantidade de numeros
+// 2 colocar em rol
+// 3 media
+// 4 mediana (impar e par)
+// 5 MODA
 
-        //VERIFICANDO QUANTIDADE DE NUMEROS
-        var quantidadeNumero = conjuntoNumero.length;
-        console.log(`O conjunto tem ${quantidadeNumero} numeros.`)
+//PRINTANDO O CONJUNTO
+var conjuntoNumero = [1, 5, 4, 7, 6, 2, 1, 5, 4, 4, 85];
 
-        //PONDO OS VALORES EM ORDEM CRESCENTE "ROL"
-        var crescente = conjuntoNumero.sort((a, b) => a - b);
-        console.log(`ROL: {${crescente}}.`)
+//VERIFICANDO QUANTIDADE DE NUMEROS
+var quantidadeNumero = conjuntoNumero.length;
 
-        //SOMA DOS VALORES DO VETOR
-        var somaNumeros = (a, b) => a + b;
-        var soma = conjuntoNumero.reduce(somaNumeros);
-        console.log(`A soma dos valores: ${soma}.`)
+console.log(`O conjunto é {${conjuntoNumero}}`)
 
-        //CALCULANDO A MEDIA
-        console.log(`Média: ${soma/quantidadeNumero}.`)
-        
-        //AMPLITUDE TOTAL
-        var indice = conjuntoNumero.length - 1;
-        var amplitude = crescente[indice] - crescente[0];
-        console.log(`Amplitude total: ${amplitude}.`)
+console.log(`O conjunto tem ${quantidadeNumero} numeros`)
 
-        if(conjuntoNumero.length%2 !== 0){
-        //MEDIANA SE FOR IMPAR (numero + 1 / 2)
-        indiceImpar = quantidadeNumero + 1 / 2; 
-        var medianaImpar = conjuntoNumero[indiceImpar];
-        console.log(`Mediana: ${medianaimpar}.`)
-        } else {
-        //MEDIANA SE FOR PAR (indice 1 = (numero / 2) + o indice 2 = (numero / 2) + 1)
-        indicePar = quantidadeNumero / 2 - 1 ;  //no codigo foi decrementado 1 por conta do indice começar em 0
-        var medianaPar = (conjuntoNumero[indicePar] + conjuntoNumero[indicePar + 1]) / 2;
-        console.log(`Mediana - ${medianaPar}.`)
-        }
+//PONDO OS VALORES EM ORDEM CRESCENTE "ROL"
+var crescente = conjuntoNumero.sort((a, b) => a - b)
+console.log(`Em ROL {${crescente}}`)
 
-        //VERIFICANDO A MODA
-        var modaConjunto = [];
-        var moda = conjuntoNumero.filter(function(elemento, i){
-            if (conjuntoNumero.indexOf(elemento) !== i){
-                modaConjunto.push(elemento)
-            }
-            return conjuntoNumero.indexOf(elemento == i)
-        })
+//SOMANDO DOS VALORES DO VETOR
+var somaNumeros = (a, b) => a + b;
+var soma = conjuntoNumero.reduce(somaNumeros)
+console.log(`A soma dos valores é ${soma}`)
 
-        console.log(modaConjunto)
+//CALCULANDO A MEDIA
+console.log(`A média aritmética é ${soma/quantidadeNumero}`)
+
+//CALCULANDO AMPLITUDE TOTAL (max - min)
+var indice = conjuntoNumero.length - 1
+var amplitude = crescente[indice] - crescente[0]
+console.log(`A amplitude total é ${amplitude}`)
+
+if (conjuntoNumero.length % 2 !== 0) {
+    //MEDIANA SE FOR IMPAR (numero + 1 / 2)
+    indiceImpar = (quantidadeNumero + 1) / 2;
+    var medianaImpar = conjuntoNumero[indiceImpar]
+    console.log(`A mediana é ${medianaImpar}`)
+} else {
+    //MEDIANA SE FOR PAR (indice 1 = (numero / 2) + o indice 2 = (numero / 2) + 1)
+    indicePar = quantidadeNumero / 2 - 1; //no codigo foi decrementado 1 por conta do indice começar em 0
+    var medianaPar = (conjuntoNumero[indicePar] + conjuntoNumero[indicePar + 1]) / 2
+    console.log(`A mediana é ${medianaPar}`)
+}
+
+//VERIFICANDO A MODA (número que mais se repete no conjunto)
+var modaConjunto = [];
+var modaFuncao = conjuntoNumero.filter(function (elemento, i) {
+    if (conjuntoNumero.indexOf(elemento) !== i) {
+        modaConjunto.push(elemento)
+    }
+    return conjuntoNumero.indexOf(elemento == i)
+})
+
+console.log(`A MODA é ${modaConjunto}`)
+var moda = [];
+var modaFuncao1 = modaConjunto.filter(function (elemento, i) {
+    if (modaConjunto.indexOf(elemento) !== i) {
+        moda.push(elemento)
+    }
+    return modaConjunto.indexOf(elemento == i)
+})
+
+console.log(`A MODA é ${moda}`)
