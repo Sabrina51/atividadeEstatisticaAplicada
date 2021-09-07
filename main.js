0a7651f4f54e61e38d09494dd4cf6b2c4c6a9f0d
sabrina51 <sabrina.silva51@fatec.sp.gov.br>
2021-09-07 20:18:08 -0300

    add validations and formatting output text

--- main.js
+++ main.js
@@ -4,61 +4,89 @@
 // 4 mediana (impar e par)
 // 5 MODA
 
-//PRINTANDO O CONJUNTO
-var conjuntoNumero = [1, 5, 4, 7, 6, 2, 1, 5, 4, 4, 85];
+var domElementoConjuntoNumero = document.querySelector('#conjunto_numeros')
+var conjuntoNumero = []
 
-//VERIFICANDO QUANTIDADE DE NUMEROS
-var quantidadeNumero = conjuntoNumero.length;
+function adicionar() {
+    if (domElementoConjuntoNumero.value < 0 || domElementoConjuntoNumero.value > 9999 || domElementoConjuntoNumero.value === "") {
+        alert('[ERRO] Caixa em branco ou número inválido!')
+    } else {
+        var numero = Number(domElementoConjuntoNumero.value)
+        conjuntoNumero.push(numero)
+        console.log(conjuntoNumero)
 
-console.log(`O conjunto é {${conjuntoNumero}}`)
-
-console.log(`O conjunto tem ${quantidadeNumero} numeros`)
-
-//PONDO OS VALORES EM ORDEM CRESCENTE "ROL"
-var crescente = conjuntoNumero.sort((a, b) => a - b)
-console.log(`Em ROL {${crescente}}`)
-
-//SOMANDO DOS VALORES DO VETOR
-var somaNumeros = (a, b) => a + b;
-var soma = conjuntoNumero.reduce(somaNumeros)
-console.log(`A soma dos valores é ${soma}`)
-
-//CALCULANDO A MEDIA
-console.log(`A média aritmética é ${soma/quantidadeNumero}`)
-
-//CALCULANDO AMPLITUDE TOTAL (max - min)
-var indice = conjuntoNumero.length - 1
-var amplitude = crescente[indice] - crescente[0]
-console.log(`A amplitude total é ${amplitude}`)
-
-if (conjuntoNumero.length % 2 !== 0) {
-    //MEDIANA SE FOR IMPAR (numero + 1 / 2)
-    indiceImpar = (quantidadeNumero + 1) / 2;
-    var medianaImpar = conjuntoNumero[indiceImpar]
-    console.log(`A mediana é ${medianaImpar}`)
-} else {
-    //MEDIANA SE FOR PAR (indice 1 = (numero / 2) + o indice 2 = (numero / 2) + 1)
-    indicePar = quantidadeNumero / 2 - 1; //no codigo foi decrementado 1 por conta do indice começar em 0
-    var medianaPar = (conjuntoNumero[indicePar] + conjuntoNumero[indicePar + 1]) / 2
-    console.log(`A mediana é ${medianaPar}`)
+        var domConjunto = document.getElementById('conjunto')
+        domConjunto.innerHTML = `{${conjuntoNumero}}`
+        
+    }
+    domElementoConjuntoNumero.value = ''
+    domElementoConjuntoNumero.focus();
 }
 
-//VERIFICANDO A MODA (número que mais se repete no conjunto)
-var modaConjunto = [];
-var modaFuncao = conjuntoNumero.filter(function (elemento, i) {
-    if (conjuntoNumero.indexOf(elemento) !== i) {
-        modaConjunto.push(elemento)
-    }
-    return conjuntoNumero.indexOf(elemento == i)
-})
+console.log(conjuntoNumero)
 
-console.log(`A MODA é ${modaConjunto}`)
-var moda = [];
-var modaFuncao1 = modaConjunto.filter(function (elemento, i) {
-    if (modaConjunto.indexOf(elemento) !== i) {
-        moda.push(elemento)
-    }
-    return modaConjunto.indexOf(elemento == i)
-})
 
-console.log(`A MODA é ${moda}`)
+function calcular() {
+    var domResultato = document.querySelector('#resultados')
+    if (conjuntoNumero.length === 0 || conjuntoNumero.length < 2) {
+        alert('[ERRO] Adicione dois ou mais números ao conjunto!')
+    } else {
+        //VERIFICANDO QUANTIDADE DE NUMEROS
+        var quantidadeNumero = conjuntoNumero.length;
+
+        domResultato.innerHTML +=`<p>O conjunto tem ${quantidadeNumero} numeros</p>`
+
+        //PONDO OS VALORES EM ORDEM CRESCENTE "ROL"
+        var crescente = conjuntoNumero.sort((a, b) => a - b)
+        domResultato.innerHTML +=`<p>Em ROL {${crescente}}</p>`
+
+        //SOMANDO DOS VALORES DO VETOR
+        var somaNumeros = (a, b) => a + b;
+        var soma = conjuntoNumero.reduce(somaNumeros)
+        domResultato.innerHTML +=`<p>A soma dos valores é ${soma}</p>`
+
+        //CALCULANDO A MEDIA
+        domResultato.innerHTML +=`<p>A média aritmética é ${soma/quantidadeNumero}</p>`
+
+        //CALCULANDO AMPLITUDE TOTAL (max - min)
+        var indice = conjuntoNumero.length - 1
+        var amplitude = crescente[indice] - crescente[0]
+        domResultato.innerHTML +=`<p>A amplitude total é ${amplitude}</p>`
+
+        if (conjuntoNumero.length % 2 !== 0) {
+            //MEDIANA SE FOR IMPAR (numero + 1 / 2)
+            indiceImpar = (quantidadeNumero - 1) / 2;
+            var medianaImpar = conjuntoNumero[indiceImpar]
+            domResultato.innerHTML +=`<p>A mediana é ${medianaImpar}</p>`
+        } else {
+            //MEDIANA SE FOR PAR (indice 1 = (numero / 2) + o indice 2 = (numero / 2) + 1)
+            indicePar = quantidadeNumero / 2 - 1; //no codigo foi decrementado 1 por conta do indice começar em 0
+            var medianaPar = (conjuntoNumero[indicePar] + conjuntoNumero[indicePar + 1]) / 2
+            domResultato.innerHTML +=`<p>A mediana é ${medianaPar}</p>`
+        }
+
+        //VERIFICANDO A MODA (número que mais se repete no conjunto)
+        var modaConjunto = [];
+        var modaFuncao = conjuntoNumero.filter(function (elemento, i) {
+            if (conjuntoNumero.indexOf(elemento) !== i) {
+                modaConjunto.push(elemento)
+            }
+            return conjuntoNumero.indexOf(elemento == i)
+        })
+
+        domResultato.innerHTML +=`<p>A MODA é ${modaConjunto}</p>`
+        var moda = [];
+        var modaFuncao1 = modaConjunto.filter(function (elemento, i) {
+            if (modaConjunto.indexOf(elemento) !== i) {
+                moda.push(elemento)
+            }
+            return modaConjunto.indexOf(elemento == i)
+        })
+
+        domResultato.innerHTML +=`<p>A MODA é ${moda}</p>`
+    }
+}
+
+function limpar(){
+    alert('limpar')
+}
